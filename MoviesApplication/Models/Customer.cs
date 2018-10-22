6512530701f6a63f7f05b7fda1679a52e80436aa
Piotr Surdyn <piotrsurdyn@gmail.com>
2018-10-22 17:44:37 +0200

    created base model classes

---
+++ MoviesApplication/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoviesApplication.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+    }
+}
---
+++ MoviesApplication/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MoviesApplication.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public DateTime ReleasedDate { get; set; }
+        public Genre Genre { get; set; }
+
+        [ForeignKey("Genre")]
+        public int GenreId { get; set; }
+    }
+}
